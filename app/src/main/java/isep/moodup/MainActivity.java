fe11f69c5e2b41e6f6a4486d30146732c6a98a38
karimberrahil <karim.berrahil@isep.fr>
2016-12-11 16:50:31 +0100

    ajout map

--- app/src/main/java/isep/moodup/MainActivity.java
+++ app/src/main/java/isep/moodup/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button buttonAdd;
     private Button buttonView;
+    private Button buttonMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +20,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonAdd = (Button) findViewById(R.id.buttonAdd);
         buttonView = (Button) findViewById(R.id.buttonView);
+        buttonMap = (Button) findViewById(R.id.buttonMap);
 
         //Setting listeners to button
         buttonAdd.setOnClickListener(this);
         buttonView.setOnClickListener(this);
+        buttonMap.setOnClickListener(this);
     }
 
     @Override
@@ -30,8 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == buttonAdd) {
             startActivity(new Intent(this, AddIncident.class));
         }
-        if (v == buttonView) {
+        else if (v == buttonView) {
             startActivity(new Intent(this, ViewAllIncident.class));
+        }
+        else if (v == buttonMap) {
+            startActivity(new Intent(this, MapsActivity.class));
         }
     }
 }
